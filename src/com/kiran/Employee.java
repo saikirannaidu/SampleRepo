fdf4a431c7720918607e4640a2c46f2d44e7922a
ammu <ammu@SATISH>
2018-05-12 14:28:10 +0530

    adding to repo

---
+++ src/com/kiran/Employee.java
@@ -0,0 +1,26 @@
+package com.kiran;
+
+public class Employee {
+private int empId;
+private String empName;
+public Employee() {
+	super();
+}
+public int getEmpId() {
+	return empId;
+}
+public void setEmpId(int empId) {
+	this.empId = empId;
+}
+public String getEmpName() {
+	return empName;
+}
+public void setEmpName(String empName) {
+	this.empName = empName;
+}
+@Override
+public String toString() {
+	return "Employee [empId=" + empId + ", empName=" + empName + "]";
+}
+
+}
